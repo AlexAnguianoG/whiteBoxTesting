5bdcd2c0b3c09c04184f37f077e797303049b0cd
Alejandro Aguiano <alex1203@live.com.mx>
2021-04-27 16:39:47 -0600

    setup

---
+++ src/test/java/com/tec/lab/WhiteBoxTestingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tec.lab;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WhiteBoxTestingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
